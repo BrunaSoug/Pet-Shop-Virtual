a35b99c14a435adcf735ea713f570d85c2514298
BrunaSoug <brunasoug@gmail.com>
2024-08-18 21:51:17 -0300

    Adicionado arquivos de preparação de ambiente

---
+++ WebDev-4/src/main/java/br/edu/iff/ccc/bsi/webdev/WebDev4Application.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDev4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDev4Application.class, args);
+	}
+
+}
---
+++ WebDev-4/src/test/java/br/edu/iff/ccc/bsi/webdev/WebDev4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.webdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDev4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
