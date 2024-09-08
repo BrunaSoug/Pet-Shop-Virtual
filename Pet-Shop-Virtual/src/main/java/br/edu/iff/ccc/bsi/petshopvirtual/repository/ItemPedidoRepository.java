1936903a3006ba5bc2be4cc7324b43c4b75f6a7a
Messias da Silva <messias.silva.developer@gmail.com>
2024-09-08 15:02:10 -0300

    Implemento de consultas personalizadas

--- Pet-Shop-Virtual/src/main/java/br/edu/iff/ccc/bsi/petshopvirtual/repository/ItemPedidoRepository.java
+++ Pet-Shop-Virtual/src/main/java/br/edu/iff/ccc/bsi/petshopvirtual/repository/ItemPedidoRepository.java
@@ -1,10 +1,16 @@
 package br.edu.iff.ccc.bsi.petshopvirtual.repository;
 
 import br.edu.iff.ccc.bsi.petshopvirtual.entities.ItemPedido;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-    
+    @Query("SELECT ip FROM ItemPedido ip WHERE ip.pedido.numeroPedido = :numeroPedido")
+    List<ItemPedido> findByPedidoNumeroPedido(@Param("numeroPedido") String numeroPedido);
 }
